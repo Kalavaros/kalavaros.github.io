61b8a5e5707cd47f4b1243ea94c53a297de6f58e
kalavaros <marek.milicka@gmail.com>
2022-07-26 13:48:14 +0200

    first steps!

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  Hello world
+
+
+  <audio controls autoplay id="playAudio">
+    <source src="videoplayback.mp3">
+  </audio>
+  <?php
+  parent.window.location.reload();
+  ?>
+</html>
