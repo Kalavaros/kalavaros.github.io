735375174400b24e9871c8cfa3dacaa2b0b546fe
kalavaros <marek.milicka@gmail.com>
2022-08-29 22:15:36 +0200

    Added text to speech functionality

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # How to run
 1. Clone repository
 2. run `php -S 127.0.0.1:8000 -c php.ini` in the dir to run a php dev server on [127.0.0.1:8000](127.0.0.1:8000)
+3. To run text to read, don't forget to add your google api key at t2s.php, line 28. Due to the projects purpose, the language is set on Czech by default. 
 # Explanation
 The main page (index.php) is for uploading music (goes to `uploads` folder) and has a testing player. The [radio page](127.0.0.1:8000/radio.php) is playing (may need to have autoplay enabled). Gives query every second to refresh.
 # Possible features
---
+++ t2s.php
@@ -0,0 +1,65 @@
+<?php
+function getSound($text)
+        {            
+            $text = trim($text);
+
+            if($text == '') return false;
+            
+            $params = [
+                "audioConfig"=>[
+                    "audioEncoding"=>"LINEAR16",
+                    "pitch"=> "1",
+                    "speakingRate"=> "1",
+                    "effectsProfileId"=> [
+                        "medium-bluetooth-speaker-class-device"
+                      ]
+                ],
+                "input"=>[
+                    "text"=>$text
+                ],
+                "voice"=>[
+                    "languageCode"=> "cs-CZ",
+                    "name" =>"cs-CZ-Wavenet-A"
+                ]
+            ];
+
+            $data_string = json_encode($params);
+
+            $speech_api_key = ''; //!!!INSERT YOUR API KEY!!!
+
+            $url = 'https://texttospeech.googleapis.com/v1/text:synthesize?fields=audioContent&key=' . $speech_api_key;
+            $handle = curl_init($url);
+            
+            curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "POST"); 
+            curl_setopt($handle, CURLOPT_POSTFIELDS, $data_string);  
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($handle, CURLOPT_HTTPHEADER, [                                                                          
+                'Content-Type: application/json',                                                                                
+                'Content-Length: ' . strlen($data_string)
+                ]                                                                       
+            );
+            $response = curl_exec($handle);              
+            $responseDecoded = json_decode($response, true);  
+            curl_close($handle);
+            if($responseDecoded['audioContent']){
+                return $responseDecoded['audioContent'];                
+            } 
+
+            return false;  
+        }
+
+function saveSound($text)
+   {
+      $speech_data = getSound($text);//see method upper
+
+      if($speech_data) {             
+         $file_name = 't2s.mp3';
+         $path = 'uploads/';
+         if(file_put_contents($path.$file_name, base64_decode($speech_data))){
+             return $file_name;
+             }
+         }
+
+        return null;
+   }
+?>
